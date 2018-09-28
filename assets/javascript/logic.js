4518b41561300fa82a04ec2655437b616071a041
enguyen93 <enguyenusaf93@gmail.com>
2018-09-28 13:52:51 -0500

    some changes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,8 +6,8 @@ var config = {
     projectId: "trainschedule-35128",
     storageBucket: "trainschedule-35128.appspot.com",
     messagingSenderId: "915094527989"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
@@ -16,23 +16,28 @@ var destinationInput = $("#destinationInput").val().trim();
 var trainTimeInput = $("#trainTimeInput").val().trim();
 var frequencyInput = $("#frequencyInput").val().trim();
 
-
 $("#submit-Btn").on("click", function (event) {
     event.preventDefault();
 
     // Grabbed values from text-boxes
     trainNameInput = $("#trainNameInput").val().trim();
     destinationInput = $("#destinationInput").val().trim();
-    trainTimeInput = $("#trainTimeInput").val().trim();
+    trainTimeInput = $("#trainTimeInput").val().trim(), "HH:mm".subtract(10, "years").format("X");
     frequencyInput = $("#frequencyInput").val().trim();
-    // Code for "Setting values in the database"
-    database.ref().push({
+
+
+    // Creates local "temporary" object for holding employee data
+    var newTrain = {
         train: trainNameInput,
         destination: destinationInput,
         arrivalTime: trainTimeInput,
         frequency: frequencyInput,
-        // minutesAway: trainMinutes
-    });
+    };
+
+
+    // Code for "Setting values in the database"
+    database.ref().push(newTrain);
+        
     $("#trainNameInput").val("");
     $("#destinationInput").val("");
     $("#trainTimeInput").val("");
@@ -42,10 +47,20 @@ $("#submit-Btn").on("click", function (event) {
 database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot) {
     // storing the snapshot.val() in a variable for convenience
     var sv = snapshot.val();
-    var arrivalTime = sv.arrivalTime;
-    var arrivalTimePretty = moment.unix(arrivalTime).format("HH/mm");
-    console.log(moment().diff(moment(arrivalTime).toNow(), "hours", "minutes"));
 
+    var firebaseName = sv.train;
+    var firebaseDest = sv.destination;
+    var firebaseArrivalTime = sv.arrivalTime;
+    var firebaseFrequency = sv.frequency;
+
+
+
+    var tRemainder = moment().diff(moment.unix(firebaseArrivalTime), "minutes") % firebaseFrequency;
+    console.log(tRemainder);
+    var minutesAway = firebaseFrequency - tRemainder;
+    console.log(minutesAway);
+    var tArrival = moment().add(minutesAway, "m").format("hh:mm");
+    console.log(tArrival);
     // Console.logging the last user's data
     console.log(sv.train);
     console.log(sv.destination);
@@ -53,17 +68,15 @@ database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", functi
     console.log(sv.frequency);
     console.log(sv.minutesAway);
 
-    
-
 
     var newRow = $("<tr>").append(
-        $("<td>").text(sv.train),
-        $("<td>").text(sv.destination),
-        $("<td>").text(sv.frequency),
-        $("<td>").text(sv.arrivalTimePretty),
-        $("<td>").text(sv.minutesAway)
-      );
-      $(".trainTable > tbody").append(newRow);
+        $("<td>").text(firebaseName),
+        $("<td>").text(firebaseDest),
+        $("<td>").text(firebaseFrequency),
+        $("<td>").text(tArrival),
+        $("<td>").text(minutesAway)
+    );
+    $(".trainTable > tbody").append(newRow);
 
     // Handle the errors
 }, function (errorObject) {
